2be192b4e9654026988698c10858d9aaaab56d60
tlaceby <tylerbadgertutorials@gmail.com>
2024-04-24 22:25:45 -0600

    Finally got code working :)

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/2Wy-Iis-)
+
 # Dijkstra's Algorithm
 
 Recall the pseudocode for Dijkstra's algorithm:
+
 - initialize the dist to each vertex to $\infty$, source to 0
 - while there are unmarked vertices left in the graph
-    - select the unmarked vertex $v$ with the lowest dist
-    - mark $v$ with distance dist
-    - for each edge $(v,w)$
-        - dist($w$) = min $\left(\textrm{dist}(w), \textrm{dist}(v) + \textrm{weight of }(v, w)\right)$
+  - select the unmarked vertex $v$ with the lowest dist
+  - mark $v$ with distance dist
+  - for each edge $(v,w)$
+    - dist($w$) = min $\left(\textrm{dist}(w), \textrm{dist}(v) + \textrm{weight of }(v, w)\right)$
 
 Implement Dijkstra's algorithm. Start with the template I provided in `code.js`
 and test your new function.
@@ -24,3 +26,7 @@ inefficient.
 
 What is the big $\Theta$ complexity of your implementation? Add your
 answer, including your reasoning, to this markdown file.
+
+## Resources
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
--- code.js
+++ code.js
@@ -1,3 +1,46 @@
-function dijkstra(graph, sourceNode) {
-    return [];
+// - initialize the dist to each vertex to $\infty$, source to 0
+// - while there are unmarked vertices left in the graph
+//     - select the unmarked vertex $v$ with the lowest dist
+//     - mark $v$ with distance dist
+//     - for each edge $(v,w)$
+//         - dist($w$) = min $\left(\textrm{dist}(w), \textrm{dist}(v) + \textrm{weight of }(v, w)\right)$
+
+/**
+ * @param {number[][]} graph
+ * @param {number} start
+ */
+function dijkstra(graph, start) {
+  // initialize the dist to each vertex to $\infty$, source to 0
+  let distances = new Array(graph.length).fill(Infinity);
+  let visited = new Set();
+  distances[start] = 0;
+
+  // while there are unmarked vertices left in the graph
+  while (visited.size < graph.length) {
+    //  select the unmarked vertex $v$ with the lowest dist
+    let [minDistance, closestNode] = distances.reduce(
+      ([min, minIdx], distance, idx) =>
+        !visited.has(idx) && distance < min ? [distance, idx] : [min, minIdx],
+      [Infinity, null]
+    );
+
+    // Remaining nodes are unreachable/
+    if (minDistance === Infinity) break;
+    visited.add(closestNode);
+
+    for (let [neighbor, weight] of graph[closestNode]) {
+      if (!visited.has(neighbor)) {
+        let newDistance = distances[closestNode] + weight;
+        if (newDistance < distances[neighbor]) {
+          distances[neighbor] = newDistance;
+        }
+      }
+    }
+  }
+
+  return distances;
 }
+
+module.exports = {
+  dijkstra,
+};
